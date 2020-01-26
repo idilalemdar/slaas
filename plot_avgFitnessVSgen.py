88009b77d7da816faf3f3f93d41b7b610a9b329a
İdil Zeynep Alemdar <idilzeynepalemdar@gmail.com>
2020-01-26 13:30:46 +0300

    chore: directories arranged

--- plot_avgFitnessVSgen.py
+++ plot_avgFitnessVSgen.py
@@ -6,8 +6,8 @@ import functools
 import numpy as np
 import pandas as pd
 
-DIR1 = "thirdExperimentBatch"
-DIR2 = "fourthExperimentBatch"
+DIR1 = "experimentResults/firstExperimentBatch"
+DIR2 = "experimentResults/secondExperimentBatch"
 
 
 def extractFitnesses(dir):
@@ -16,18 +16,18 @@ def extractFitnesses(dir):
         if fname != "config.txt":
             with open(dir + "/" + fname) as fd:
                 data = fd.read().strip().split("\n")
-                data = list(filter(lambda x: x.startswith("Fitness"), data))
+                data = list(filter(lambda x: x.startswith("Total"), data))
                 data = list(map(lambda x: x[x.find(":") + 2:], data))
                 data = list(map(float, data))
                 fitnesses.append(data)
     return fitnesses
 
 
-def cumulativeFitness(fitnesses):
+def cumulativeFitness(fitnesses, pop_size):
     cumulativeFitnesses = [0 for _ in range(20)]
     for f in fitnesses:
         for j in range(20):
-            cumulativeFitnesses[j] += f[j]
+            cumulativeFitnesses[j] += f[j] / pop_size
     return list(map(lambda x: x/500, cumulativeFitnesses))
 
 
@@ -35,15 +35,16 @@ if __name__ == '__main__':
     fitnesses1 = extractFitnesses(DIR1)
     fitnesses2 = extractFitnesses(DIR2)
 
-    cumulativeFitnesses1 = cumulativeFitness(fitnesses1)
-    cumulativeFitnesses2 = cumulativeFitness(fitnesses2)
+    cumulativeFitnesses1 = cumulativeFitness(fitnesses1, 10)
+    cumulativeFitnesses2 = cumulativeFitness(fitnesses2, 50)
 
     df = pd.DataFrame({'Generations': range(0, 20), 'Population = 10': cumulativeFitnesses1, 'Population = 50': cumulativeFitnesses2 })
 
     plt.plot('Generations', 'Population = 10', data=df, marker='', color='red', linewidth=2)
     plt.plot('Generations', 'Population = 50', data=df, marker='', color='blue', linewidth=2)
     plt.legend()
-    plt.title("Best fitness vs Generation with λ1 = 0.3, λ2 = 1, μ1 = 2, μ2 = 3")
+    plt.title("Population fitness vs Generation with λ1 = 0.5, λ2 = 2, μ1 = 2, μ2 = 10")
     plt.xlabel("Generations")
-    plt.ylabel("Best fitness (normalized)")
-    plt.savefig("plot5.png")
+    plt.ylabel("Average population fitness (normalized)")
+    plt.savefig("report/plot1.png")
+
--- plot_bestFitnessVSgen.py
+++ plot_bestFitnessVSgen.py
@@ -6,8 +6,8 @@ import functools
 import numpy as np
 import pandas as pd
 
-DIR1 = "firstExperimentBatch"
-DIR2 = "secondExperimentBatch"
+DIR1 = "experimentResults/thirdExperimentBatch"
+DIR2 = "experimentResults/fourthExperimentBatch"
 
 
 def extractFitnesses(dir):
@@ -16,18 +16,18 @@ def extractFitnesses(dir):
         if fname != "config.txt":
             with open(dir + "/" + fname) as fd:
                 data = fd.read().strip().split("\n")
-                data = list(filter(lambda x: x.startswith("Total"), data))
+                data = list(filter(lambda x: x.startswith("Fitness"), data))
                 data = list(map(lambda x: x[x.find(":") + 2:], data))
                 data = list(map(float, data))
                 fitnesses.append(data)
     return fitnesses
 
 
-def cumulativeFitness(fitnesses, pop_size):
+def cumulativeFitness(fitnesses):
     cumulativeFitnesses = [0 for _ in range(20)]
     for f in fitnesses:
         for j in range(20):
-            cumulativeFitnesses[j] += f[j] / pop_size
+            cumulativeFitnesses[j] += f[j]
     return list(map(lambda x: x/500, cumulativeFitnesses))
 
 
@@ -35,16 +35,15 @@ if __name__ == '__main__':
     fitnesses1 = extractFitnesses(DIR1)
     fitnesses2 = extractFitnesses(DIR2)
 
-    cumulativeFitnesses1 = cumulativeFitness(fitnesses1, 10)
-    cumulativeFitnesses2 = cumulativeFitness(fitnesses2, 50)
+    cumulativeFitnesses1 = cumulativeFitness(fitnesses1)
+    cumulativeFitnesses2 = cumulativeFitness(fitnesses2)
 
     df = pd.DataFrame({'Generations': range(0, 20), 'Population = 10': cumulativeFitnesses1, 'Population = 50': cumulativeFitnesses2 })
 
     plt.plot('Generations', 'Population = 10', data=df, marker='', color='red', linewidth=2)
     plt.plot('Generations', 'Population = 50', data=df, marker='', color='blue', linewidth=2)
     plt.legend()
-    plt.title("Population fitness vs Generation with λ1 = 0.5, λ2 = 2, μ1 = 2, μ2 = 10")
+    plt.title("Best fitness vs Generation with λ1 = 0.3, λ2 = 1, μ1 = 2, μ2 = 3")
     plt.xlabel("Generations")
-    plt.ylabel("Average population fitness (normalized)")
-    plt.savefig("plot1.png")
-
+    plt.ylabel("Best fitness (normalized)")
+    plt.savefig("report/plot5.png")
